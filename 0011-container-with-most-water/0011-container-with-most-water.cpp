4a0fd45ced7045f11c7edb25810f91112c81ea4d
liptan <liptankumar465@gmail.com>
2026-07-17 23:46:33 +0530

    Time: 0 ms (100%), Space: 63.1 MB (11.1%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -6,13 +6,15 @@ public:
         int right = height.size() - 1;
 
         while (left < right) {
-
+            // calculate current width and min height.
             int width = right - left;
             int current_height = min(height[left], height[right]);
-
+            
+            // update the maximum water found so far.
             int current_water = width * current_height;
             max_water = max(max_water, current_water);
 
+            // move the pointer that points to the shorter line.
             if (height[left] < height[right]) {
                 left++;
             } else {

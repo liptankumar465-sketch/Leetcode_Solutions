923273d5f78e153f21f6038b71d6cebd092db488
liptan <liptankumar465@gmail.com>
2026-07-19 17:36:29 +0530

    Time: 4 ms (56.76%), Space: 26.8 MB (15.17%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+
+public:
+    bool palindromeOrNot(int left, int right, string s) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+public:
+    bool validPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s[left] != s[right]) {
+                // use the supperPower
+                return palindromeOrNot(left + 1, right, s) ||
+                       palindromeOrNot(left, right - 1, s);
+            } else {
+                left++;
+                right--;
+            }
+        }
+       return true;
+    }
+};

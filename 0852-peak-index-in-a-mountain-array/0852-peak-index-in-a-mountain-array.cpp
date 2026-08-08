f3f7ac0d97dae5a0b2d48bd8166a107014d98388
liptan <liptankumar465@gmail.com>
2026-08-08 23:15:56 +0530

    Time: 0 ms (100%), Space: 63.6 MB (49.1%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -3,24 +3,15 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
 
         int peck_element = INT_MIN;
-        int low = 0, high = arr.size() - 1;
 
-        for(auto mountain : arr) {
+        for (auto mountain : arr) {
             peck_element = max(mountain, peck_element);
         }
-        
-        for(int i=0;i<arr.size();i++) {
-            if (arr[i] == peck_element) return i;
+
+        for (int peck_index = 0; peck_index < arr.size(); peck_index++) {
+            if (arr[peck_index] == peck_element)
+                return peck_index;
         }
-        // while(low < high) {
-
-        //     int peck_index = low + (high-low)/2;
-            
-        //     if (arr[peck_index] == peck_element) return peck_index;
-        //     else if (arr[peck_index] < peck_element) low = peck_index + 1;
-        //     else high = peck_index - 1;
-
-        // }
 
         return -1;
     }

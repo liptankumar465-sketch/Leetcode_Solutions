7acb367b515669cb098e1e484d071fb012f60b96
liptan <liptankumar465@gmail.com>
2026-08-10 17:09:21 +0530

    Time: 49 ms (49.4%), Space: 28.9 MB (92.72%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
-        vector<vector<int>> ans;
+        vector<vector<int>> result;
         int n = nums.size();
-        
-        // Step 1: Sort the array
+        // first sort the nums
         sort(nums.begin(), nums.end());
-        
-        for (int i = 0; i < n - 2; ++i) {
-            // Early exit: if the smallest fixed number is positive, 
-            // no three numbers can sum to 0
-            if (nums[i] > 0) break;
-            
-            // Skip duplicate outer anchors
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            
-            int l = i + 1;
-            int r = n - 1;
-            
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                
+        // fixed the one index
+        for (int i = 0; i < n - 2; i++) {
+            // ignore the duplicates for i
+            if (i > 0 && nums[i] == nums[i - 1]) 
+                continue;
+            // first index element can't be possitive
+            if (nums[i] > 0)
+                break;
+            // aother pointers
+            int left = i + 1, right = n - 1;
+
+            while (left < right) {
+                long sum = (long)nums[i] + nums[left] + nums[right];
+
                 if (sum == 0) {
-                    ans.push_back({nums[i], nums[l], nums[r]});
+                    result.push_back({nums[i], nums[left], nums[right]});
                     
-                    // Skip duplicates for the left pointer
-                    while (l < r && nums[l] == nums[l + 1]) l++;
-                    // Skip duplicates for the right pointer
-                    while (l < r && nums[r] == nums[r - 1]) r--;
-                    
-                    // Move pointers to the next new elements
-                    l++;
-                    r--;
+                    // ignore the duplicates for left
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    // ignore the duplicates for right
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
                 } else if (sum < 0) {
-                    l++; // Need a larger sum
+                    left++;
                 } else {
-                    r--; // Need a smaller sum
+                    right--;
                 }
             }
         }
-        
-        return ans;
 
+        return result;
     }
 };

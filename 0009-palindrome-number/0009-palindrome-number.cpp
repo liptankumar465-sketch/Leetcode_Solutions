b8b353f5e9ae7c330ed346cda7a49dd693a43809
liptan <liptankumar465@gmail.com>
2026-08-10 17:49:18 +0530

    Time: 4 ms (30.83%), Space: 10.9 MB (7.17%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,31 +1,48 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        vector<int> givenNum;
+        // firsr convert given x into string givenNum
+        // by to_string() IN-BUILD FUNCTION
+        string givenNum = to_string(x);
+        int n = givenNum.size(); // size
 
-        if (x < 0) return false;
-        
-        while (x > 0) {
-            int lastDigit = x % 10;
-            x = x/10;
-            givenNum.push_back(lastDigit);
-        }
-
-        int n = givenNum.size();
-
+        // creat two pointers to compare x is palindrome or not
         int left = 0, right = n - 1;
 
         while (left < right) {
-
-            if (givenNum[left] != givenNum[right]) {
+            if (givenNum[left] != givenNum[right])
                 return false;
-
-            } else {
+            else
                 left++;
-                right--;
-            }
+            right--;
         }
-
         return true;
+        
+        //---------brute force examp---------//
+        // vector<int> givenNum;
+
+        // if (x < 0) return false;
+
+        // while (x > 0) {
+        //     int lastDigit = x % 10;
+        //     x = x/10;
+        //     givenNum.push_back(lastDigit);
+        // }
+
+        // int n = givenNum.size();
+
+        // int left = 0, right = n - 1;
+
+        // while (left < right) {
+
+        //     if (givenNum[left] != givenNum[right]) {
+        //         return false;
+
+        //     } else {
+        //         left++;
+        //         right--;
+        //     }
+        // }
+        // return true;
     }
 };

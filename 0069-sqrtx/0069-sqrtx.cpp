9b9737ee51b7b47680793e15b615b1b972b2b239
liptan <liptankumar465@gmail.com>
2026-08-20 23:03:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.51%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -5,7 +5,7 @@ public:
         int ans = -1;
 
         if (x < 2) return x;
-        
+
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
             if (mid == x / mid)
@@ -18,5 +18,20 @@ public:
         }
 
         return ans;
+
+        // //-----------brute force-------------//
+        // int ans = 0;
+        // if (x < 2)
+        //     return x;
+
+        // for (int i = 1; i <= x / 2; i++) {
+        //     if (i == x / i) {
+        //         return i;
+        //     }
+        //     if (i > x / i)
+        //         return i - 1;
+        // }
+
+        // return ans;
     }
 };

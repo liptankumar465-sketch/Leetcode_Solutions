2102241ee97392cb0b72f31fd74e9b076cfbbf91
liptan <liptankumar465@gmail.com>
2026-08-20 21:30:22 +0530

    Time: 0 ms (100%), Space: 63.5 MB (48.98%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        //-----------brute force--------------//
-        int peck_element = INT_MIN;
-        int ans = -1;
+        int lo = 0, hi = arr.size() - 1, ans = -1;
 
-        for (int peck_index = 0; peck_index < arr.size(); peck_index++) {
-            if (arr[peck_index] > peck_element) {
-                peck_element = arr[peck_index];
-                ans = peck_index;
+        while (lo <= hi) {
+            int mid = hi + (lo - hi) / 2; // this is another way to find mid
+            // this is not pass runtime error
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
             }
+            // right side move condition
+            else if (arr[mid] < arr[mid + 1]) {
+                ans = mid;
+                lo = mid + 1;
+            }
+            // left  side move condition
+            else
+                hi = mid - 1;
         }
 
         return ans;
+
+        // //-----------brute force--------------//
+        // int peck_element = INT_MIN;
+        // int ans = -1;
+
+        // for (int peck_index = 0; peck_index < arr.size(); peck_index++) {
+        //     if (arr[peck_index] > peck_element) {
+        //         peck_element = arr[peck_index];
+        //         ans = peck_index;
+        //     }
+        // }
+
+        // return ans;
     }
 };

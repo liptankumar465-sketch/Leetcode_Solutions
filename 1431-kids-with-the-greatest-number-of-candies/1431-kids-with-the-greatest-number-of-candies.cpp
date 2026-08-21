c1154e317410a33a19ddb6c5fd289dae8ac4e13a
liptan <liptankumar465@gmail.com>
2026-08-21 23:50:49 +0530

    Time: 0 ms (100%), Space: 12.5 MB (60.35%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        vector<bool> ans;
-        
-        int max = candies[0];
-        for (int i = 0; i < candies.size(); i++) {
-            if (candies[i] > max) {
-                max = candies[i];
-            }
+        // Find out the greatest number of candies among all the kids.
+        int maxCandies = 0;
+        for (int candy : candies) {
+            maxCandies = max(candy, maxCandies);
+        }
+        // For each kid, check if they will have greatest number of candies
+        // among all the kids.
+        vector<bool> result;
+        for (int candy : candies) {
+            result.push_back(candy + extraCandies >= maxCandies);
+            // if condition true than push -> true
+            // if condition false than push -> false
         }
 
-        for (int i = 0; i < candies.size(); i++) {
-            if (candies[i] + extraCandies < max) {
-                ans.push_back(false);
-            } else {
-                ans.push_back(true);
-            }
-        }
+        return result;
 
-        return ans;
+        ////-----------brute force-------------//
+        // find maxCandies
+        // int maxCandies = candies[0];
+        // for (int i = 0; i < candies.size(); i++) {
+        //     if (candies[i] > maxCandies) {
+        //         maxCandies = candies[i];
+        //     }
+        // }
+
+        // vector<bool> ans;
+        // for (int i = 0; i < candies.size(); i++) {
+        //     if (candies[i] + extraCandies < maxCandies) {
+        //         ans.push_back(false);
+        //     } else {
+        //         ans.push_back(true);
+        //     }
+        // }
+
+        // return ans;
     }
 };

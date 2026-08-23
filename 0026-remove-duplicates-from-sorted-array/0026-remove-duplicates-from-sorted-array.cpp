8a6e9118bcccb5d19c6f9770d46fcf2cd64dfe56
liptan <liptankumar465@gmail.com>
2026-08-23 23:23:29 +0530

    Time: 0 ms (100%), Space: 22.4 MB (98.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,38 @@
+// sol by help of two pointers
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
 
-        int size = nums.size();
-        // sol by help of two pointers
-        int i = 0;
-        int j = 1;
+        /*
+            1 2 3 3 4 -> 1 2 3 4
+              i
+              j
+        */
 
-        while (j < size) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
+        int j = 1;
+        for(int i= 1;i<nums.size();i++){
+            if (nums[i] !=nums[i-1]){
+                nums[j] = nums[i];
+                j++;
             }
-            j++;
         }
 
-        return i + 1 ;
+        return j;
+
+        
+        // int n = nums.size();
+        // int left = 0;
+        // int right = 1;
+
+        // while (right < n) {
+
+        //     if (nums[left] != nums[right]) {
+        //         left++;
+        //         nums[left] = nums[right];
+        //     }
+        //     right++;
+        // }
+
+        // return left + 1;
     }
 };

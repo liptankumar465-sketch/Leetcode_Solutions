7e22c0d804071e779a038781e623c9c70083e4b3
liptan <liptankumar465@gmail.com>
2026-08-25 23:40:07 +0530

    Time: 3 ms (27.67%), Space: 97.4 MB (25.85%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int maxProfit = 0;
-        int minPrices = prices[0];
+        // 7 1 5 3 6 4
+        //           p
+        //--------------
+        // minpro = 7,1
+        // maxpro = 0,4,5
 
-        for (int i = 0; i < n; i++) {
-            if (prices[i] < minPrices) {
-                minPrices = prices[i];
-            } else {
-                int currentProfit = prices[i] - minPrices;
-                maxProfit = max(currentProfit, maxProfit);
+        int minProfit = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minProfit) {
+                minProfit = price;
+            }
+            if (price - minProfit > maxProfit) {
+                maxProfit = price - minProfit;
             }
         }
 
-        //------time limit ex---------//
+        return maxProfit;
+
+        // int n = prices.size();
+        // int maxProfit = 0;
+        // int minPrices = prices[0];
+
+        // for (int i = 0; i < n; i++) {
+        //     if (prices[i] < minPrices) {
+        //         minPrices = prices[i];
+        //     } else {
+        //         int currentProfit = prices[i] - minPrices;
+        //         maxProfit = max(currentProfit, maxProfit);
+        //     }
+        // }
+
+        // return maxProfit;
+
+        //------brute force---------//
         // int n = prices.size();
         // int maxProfit = 0;
 
@@ -27,6 +49,6 @@ public:
         //     }
         // }
 
-        return maxProfit;
+        // return maxProfit;
     }
 };

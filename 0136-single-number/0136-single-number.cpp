0ca2f26398fa6d74ff2b8d4f2e72895fbf0d8a88
liptan <liptankumar465@gmail.com>
2026-08-27 10:27:32 +0530

    Time: 2 ms (23.65%), Space: 24 MB (7.51%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,23 @@
 class Solution {
+    // int unique = 0;
+    // // for each loop
+    // for (auto val : nums) {
+    //     unique ^= val;
+    // }
+    // return unique;
 public:
     int singleNumber(vector<int>& nums) {
-        int unique = 0;
-        // for each loop
-        for (int val : nums) {
-            unique ^= val;
+        unordered_map<int, int> freq;
+
+        for (int num : nums) {
+            freq[num]++;
         }
-        return unique;
+
+        for (const auto& pairs : freq) {
+            if (pairs.second == 1)
+                return pairs.first;
+        }
+
+        return -1;
     }
 };

13ca9ecdd0481b858d566a35872525f7f5c1f65b
liptan <liptankumar465@gmail.com>
2026-09-04 22:01:03 +0530

    Time: 0 ms (100%), Space: 21.7 MB (70.91%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+// if ele has missing in nums
+// nums = [3, 0, 1], numsSum = 3+0+1 = 4
+// if ele has not missing nums
+// nums = [1, 2, 3], actualSum = 1+2+3 = 6
+// missin no = 6 - 4 = 2 return
+public:
+    int missingNumber(vector<int>& nums) {
+
+        int n = nums.size();
+        int numsSum = 0;
+        int actualSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            numsSum += nums[i];
+            actualSum += i + 1;
+        }
+
+        return actualSum - numsSum;
+    }
+};

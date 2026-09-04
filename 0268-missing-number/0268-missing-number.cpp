2a493aef949996182830573e3e48fcad11af22e4
liptan <liptankumar465@gmail.com>
2026-09-04 22:07:59 +0530

    Time: 0 ms (100%), Space: 21.7 MB (93.93%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,29 +1,29 @@
 class Solution {
-// if ele has missing in nums
-// nums = [3, 0, 1], numsSum = 3+0+1 = 4
-// if ele has not missing nums
-// nums = [1, 2, 3], actualSum = 1+2+3 = 6
-// missin no = 6 - 4 = 2 return
+    // if ele has missing in nums
+    // nums = [3, 0, 1], numsSum = 3+0+1 = 4
+    // if ele has not missing nums
+    // nums = [1, 2, 3], actualSum = 1+2+3 = 6
+    // missin no = 6 - 4 = 2 return
+
+    //--------brute force--------//
+    // int n = nums.size();
+    // int numsSum = 0;
+    // int actualSum = 0;
+
+    // for (int i = 0; i < n; i++) {
+    //     numsSum += nums[i];
+    //     actualSum += i + 1;
+    // }
+
+    // return actualSum - numsSum;
 public:
     int missingNumber(vector<int>& nums) {
-
-        // int n = nums.size();
-        // int numsSum = 0;
-        // int actualSum = 0;
-
-        // for (int i = 0; i < n; i++) {
-        //     numsSum += nums[i];
-        //     actualSum += i + 1;
-        // }
-
-        // return actualSum - numsSum;
-
-        //--------brute force--------//
-        int n =nums.size();
+        // missing = 3^0^1^1^2^3 = 2
+        int n = nums.size();
         int missing = 0;
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             missing ^= nums[i];
-            missing ^= i+1;
+            missing ^= i + 1;
         }
 
         return missing;

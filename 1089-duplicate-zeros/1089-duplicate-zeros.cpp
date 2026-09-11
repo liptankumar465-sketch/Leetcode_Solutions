3f0dfd91dae0951462c5cd1ed2b213643090d986
liptan <liptankumar465@gmail.com>
2026-09-11 09:50:16 +0530

    Time: 0 ms (100%), Space: 14.5 MB (6.78%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
+
         int n = arr.size();
         vector<int> result(n);
         int i = 0;
@@ -18,7 +19,7 @@ public:
         }
 
         for (int i = n; i < 2 * n; i++) {
-            arr[i-n] = result[i];
+            arr[i - n] = result[i];
         }
     }
 };
